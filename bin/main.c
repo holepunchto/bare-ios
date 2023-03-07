91cc31d9ae214a9250294ea4055a0a1aa4e47750
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-07 16:08:01 +0100

    Update runtime and add asserts

--- bin/main.c
+++ bin/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <pear.h>
 #include <uv.h>
 
@@ -5,15 +6,22 @@
 
 int
 main (int argc, char *argv[]) {
+  int err;
+
+  argv = uv_setup_args(argc, argv);
+
   pear_t pear;
-  pear_setup(uv_default_loop(), &pear, argc, argv);
+  err = pear_setup(uv_default_loop(), &pear, argc, argv);
+  assert(err == 0);
 
   uv_buf_t source = uv_buf_init((char *) bundle, bundle_len);
 
-  pear_run(&pear, "/main.bundle", &source);
+  err = pear_run(&pear, "/main.bundle", &source);
+  assert(err == 0);
 
   int exit_code;
-  pear_teardown(&pear, &exit_code);
+  err = pear_teardown(&pear, &exit_code);
+  assert(err == 0);
 
   return exit_code;
 }
