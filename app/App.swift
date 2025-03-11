48728c3b6b595227ad7d606374b888fa1876fdcd
Tony Gorez <gorez.tony@gmail.com>
2025-03-11 17:39:06 +0100

    Play with callkit
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- app/App.swift
+++ app/App.swift
@@ -1,22 +1,31 @@
 import BareKit
 import SwiftUI
 import UserNotifications
+import CallKit
+import OSLog
+
+let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "com.example.BareKitExample", category: "App")
 
 @main
 struct App: SwiftUI.App {
   @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
   private var worklet = Worklet()
+  private let callManager = CallManager()
 
   @Environment(\.scenePhase) private var scenePhase
 
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ContentView(callManager: callManager)
         .onAppear {
           worklet.start(name: "app", ofType: "bundle")
 
           requestPushNotificationPermission()
+            
+          DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            callManager.reportIncomingCall(uuid: UUID(), handle: "John Doe")
+          }
         }
         .onDisappear {
           worklet.terminate()
@@ -68,7 +77,68 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 }
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello SwiftUI!")
-  }
+    var callManager: CallManager
+    
+    var body: some View {
+      VStack(spacing: 20) {
+        Text("Hello SwiftUI!")
+
+        Button(action: {
+            callManager.reportIncomingCall(uuid: UUID(), handle: "John Doe")
+        }) {
+          Text("Trigger Fake Call")
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+      }
+      .padding()
+    }
+}
+
+class CallManager: NSObject, CXProviderDelegate {
+    private var provider: CXProvider
+    
+    override init() {
+        let conf = CXProviderConfiguration()
+        conf.supportsVideo = false
+        
+        provider = CXProvider(configuration: conf)
+        super.init()
+        provider.setDelegate(self, queue: nil)
+    }
+    
+    func reportIncomingCall(uuid: UUID, handle: String) {
+        let update = CXCallUpdate()
+        update.remoteHandle = CXHandle(type: .generic, value: handle)
+        update.hasVideo = false
+
+        provider.reportNewIncomingCall(with: uuid, update: update) { error in
+          if let error = error {
+              logger.error("Error reporting incoming call: \(error.localizedDescription)")
+          }
+        }
+    }
+    
+    func providerDidReset(_ provider: CXProvider) {
+        
+    }
+    
+    func provider(
+      _ provider: CXProvider,
+      perform action: CXAnswerCallAction
+    ) {
+      logger.debug("Call answered")
+      action.fulfill()
+    }
+
+    func provider(
+      _ provider: CXProvider,
+      perform action: CXEndCallAction
+    ) {
+      logger.debug("Call ended")
+      action.fulfill()
+    }
+        
 }
